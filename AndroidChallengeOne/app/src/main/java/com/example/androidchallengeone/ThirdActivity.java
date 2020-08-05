35666292ed452d70b185934f9717106df29575b3
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-08-05 17:16:57 +0700

    challenge one

--- AndroidChallengeOne/app/src/main/java/com/example/androidchallengeone/ThirdActivity.java
+++ AndroidChallengeOne/app/src/main/java/com/example/androidchallengeone/ThirdActivity.java
@@ -8,10 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ThirdActivity extends AppCompatActivity {
 
     EditText email, password;
     Button login;
+    private static final String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+    private static Pattern pattern;
+    private Matcher matcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,16 @@ public class ThirdActivity extends AppCompatActivity {
         email = findViewById(R.id.etxEmail);
         password = findViewById(R.id.etxPassword);
         login = findViewById(R.id.btn_login);
+        pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(email.getText().toString());
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(email.getText().toString().length()==0){
                     //jika form Email belum di isi / masih kosong
                     email.setError("Email diperlukan!");
+                }else if(matcher.matches() == false){
+                    email.setError("Gunakan format email");
                 }else if(password.getText().toString().length()==0){
                     //jika form Passwrod belum di isi / masih kosong
                     password.setError("Password diperlukan!");
